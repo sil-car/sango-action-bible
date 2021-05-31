3cb0fc2e740bcd92c5abad3231f4dbe091894e9e
Nate Marti <nate_marti@sil.org>
2021-05-31 11:28:24 +0100

    use odfpy instead of xml

--- update-odt-lg.py
+++ update-odt-lg.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 
-"""Programmatically set language type by paragraph in an ODT file."""
+"""
+Programmatically set language type by paragraph in an ODT file.
+"""
 
 # References:
-#- https://sodocumentation.net/python/topic/479/manipulating-xml
+#   https://github.com/eea/odfpy/wiki
 
 import re
 import shutil
 import string
 import sys
-import tempfile
-import xml.etree.ElementTree as ET
-import zipfile
 
+from odf.opendocument import load
+from odf.opendocument import OpenDocumentText
+from odf.style import Style, TextProperties
+from odf.text import P
 from pathlib import Path
 
 import hs
@@ -60,13 +63,7 @@ def count_occurrences_by_lg(text_words, regex, hs_dics):
         counts[lang_code] = 0
     for t in text_words:
         counts['words'] += 1
-        # t = regex.sub('', t.lower()) # remove punctuation
         t = t.lower()
-        # for lang_code, lg_words in wordlists.items():
-        #     if re.match(r'.*[0-9]+.*', t): # includes digits
-        #         counts[lang_code] += 1
-        #     elif t in lg_words:
-        #         counts[lang_code] += 1
         for lang_code, d in hs_dics.items():
             if not d:
                 pass
@@ -74,113 +71,57 @@ def count_occurrences_by_lg(text_words, regex, hs_dics):
                 counts[lang_code] += 1
     return counts
 
-def update_zip(file_zip, filename, xml_tree):
-    """Update a file in the given ODT ZIP file with data in the given XML tree."""
-    # Generate a temp file.
-    file_tmp = file_zip.with_suffix('.tmp')
+def update_autostyles(doc, lang_codes):
+    """
+    Update The automatic paragraph styles of the given ODT document to include
+    the given languages.
+    """
+    for lang_code in lang_codes:
+        [lg, CN] = lang_code.split('_')
+        pstyle = Style(
+            name=lang_code,
+            family="paragraph",
+        )
+        pstyle.addElement(TextProperties(language=lg, country=CN))
+    return doc
 
-    # Create a temp copy of the archive without filename.
-    with zipfile.ZipFile(file_zip, 'r') as zin:
-        with zipfile.ZipFile(file_tmp, 'w') as zout:
-            zout.comment = zin.comment # preserve the comment
-            for item in zin.infolist():
-                if item.filename != filename:
-                    zout.writestr(item, zin.read(item.filename))
+def update_paragraphs_styles(doc, hs_dics):
+    results = []
+    last_text_lang = None
+    ct = 0
+    for p in doc.body.getElementsByType(P):
+        # Show progress dots: 1 for every X paragraphs.
+        x = 50
+        ct += 1
+        if ct % x == 0:
+            sys.stdout.write('.')
+            sys.stdout.flush()
 
-    # Replace archive with the temp archive.
-    Path.unlink(file_zip)
-    Path.rename(file_tmp, file_zip)
+        # Determine language code of paragraph.
+        words = []
+        for n in p.childNodes:
+            try:
+                words.extend(n.data.split())
+            except AttributeError:
+                pass
+        if words:
+            first_words = ' '.join(words[:4])
+            lang_code = determine_language(words, last_text_lang, hs_dics)
+            if lang_code:
+                p.setAttribute('stylename', lang_code)
+        else:
+            lang_code = None
+            first_words = None
 
-    # Add filename with its new data.
-    with zipfile.ZipFile(file_zip, mode='a', compression=zipfile.ZIP_DEFLATED) as zf:
-        with zf.open('content.xml', mode='w') as content_file:
-            xml_tree.write(content_file)
+        last_text_lang = lang_code
+        results.append([f"{first_words} ...", lang_code])
+    print()
+    return doc, results
 
-def view_xml(xml_tree):
-    """Show the values of the given XML tree."""
-    root = xml_tree.getroot()
-    namespace = {
-        'office': 'urn:oasis:names:tc:opendocument:xmlns:office:1.0',
-        'style': 'urn:oasis:names:tc:opendocument:xmlns:style:1.0',
-        'text': 'urn:oasis:names:tc:opendocument:xmlns:text:1.0',
-        'fo': 'urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0',
-    }
-    intro_office = '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}'
-    intro_style = '{urn:oasis:names:tc:opendocument:xmlns:style:1.0}'
-    intro_text = '{urn:oasis:names:tc:opendocument:xmlns:text:1.0}'
-    intro_fo = '{urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0}'
-    for part in root:
-        print(part.tag)
-    auto_styles = root.find('office:automatic-styles', namespace)
-    print(auto_styles.tag, auto_styles.attrib)
-    #ET.dump(root)
-
-def update_xml(xml_tree, hs_dics):
-    """Update the paragraph styles of the given XML tree to include the given languages."""
-    root = xml_tree.getroot()
-    intro_office = '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}'
-    intro_style = '{urn:oasis:names:tc:opendocument:xmlns:style:1.0}'
-    intro_text = '{urn:oasis:names:tc:opendocument:xmlns:text:1.0}'
-    intro_fo = '{urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0}'
-
-    # print("All p's:")
-    # for i in root.findall(f'./{intro_office}body/{intro_text}p'):
-    # for i in root.findall(f'./{intro_office}body'):
-    #     print(i)
-    for part in root:
-        # Create new paragraph styles for each input language code.
-        if part.tag == f"{intro_office}automatic-styles":
-            for lang_code in hs_dics.keys():
-                [lg, CN] = lang_code.split('_')
-                p_style = ET.SubElement(part, f"{intro_style}style")
-                p_style.set(f"{intro_style}name", lang_code)
-                p_style.set(f"{intro_style}family", "paragraph")
-                # p_style.set(f"{intro_style}parent-style-name", "Preformatted_20_Text")
-                p_style.set(f"{intro_style}parent-style-name", "Standard")
-                p_text = ET.SubElement(p_style, f"{intro_style}text-properties")
-                p_text.set(f"{intro_fo}language", lg)
-                p_text.set(f"{intro_fo}country", CN)
-                #p2_text.set('{http://openoffice.org/2009/office}paragraph-rsid': '00169b6b')
-
-        # Grab text from each paragraph and determine its language code.
-        if part.tag == f"{intro_office}body":
-            for paragraphs in part:
-                results = []
-                last_text_lang = None
-                ct = 0
-                for p in paragraphs:
-                    # Show progress dots: 1 for every X paragraphs.
-                    x = 50
-                    ct += 1
-                    if ct % x == 0:
-                        sys.stdout.write('.')
-                        sys.stdout.flush()
-
-                    # Determine language code of paragraph.
-                    # if p.attrib and p.text:
-                    # TODO: First line of each page is not recognized.
-                    if p.text:
-                        words = p.text.split()
-                        first_words = ' '.join(words[:4])
-                        lang_code = determine_language(words, last_text_lang, hs_dics)
-                        if lang_code:
-                            p.set(f"{intro_text}style-name", lang_code)
-                    else:
-                        # print(dir(p))
-                        # print(p.tag)
-                        # print(p.items)
-                        # print(p.getchildren())
-                        # exit()
-                        lang_code = None
-                        first_words = None
-
-                    last_text_lang = lang_code
-                    results.append([f"{first_words} ...", lang_code])
-                print()
-    return xml_tree, results
-
-def print_summary(results):
-    """Print summary statistics about number of paragraphs found for each language code."""
+def print_summary(results, hs_dics):
+    """
+    Print summary statistics about number of paragraphs found for each language code.
+    """
     total_p_ct = len(results)
     blank_p_ct = len([r for r in results if r[1] == None])
     sp = ' '*3
@@ -192,10 +133,14 @@ def print_summary(results):
         p_ct_by_lang[lang_code] = ct
         p_ct_unknown -= ct
         print(f"{sp}{ct} are {lang_code}")
+    # for r in results:
+    #     print(r[0])
     print(f"{sp}{p_ct_unknown} are unknown.")
 
-def print_results(results, start=0, end=-1):
-    """Print language code and initial paragraph text for the given range."""
+def print_results(results, hs_dics, start=0, end=-1):
+    """
+    Print language code and initial paragraph text for the given range.
+    """
     print()
     total_p_ct = len(results)
     #digit_ct = len(total_p_ct)
@@ -203,64 +148,58 @@ def print_results(results, start=0, end=-1):
         if r[1] is not None:
             print(f"{i+1+start}. {r[1]}: {r[0]}")
 
+def main():
+    # Define global variables.
+    infile = ''
+    outfile = ''
+    languages = ['en_US', 'fr_FR', 'sg_CF']
+    repo_root = Path(__file__).resolve().parents[0]
+    dict_dir = repo_root / 'dict'
 
-# Define global variables.
-infile = ''
-outfile = ''
-languages = ['en_US', 'fr_FR', 'sg_CF']
-repo_root = Path(__file__).resolve().parents[0]
-dict_dir = repo_root / 'dict'
+    # Ensure that a file was passed as an argument.
+    if len(sys.argv) > 1 and Path(sys.argv[1]).suffix == '.odt':
+        infile = Path(sys.argv[1])
+    else:
+        print("Error: Need to pass an ODT file as the first argument.")
+        exit(1)
 
-# Ensure that a file was passed as an argument.
-if len(sys.argv) > 1 and Path(sys.argv[1]).suffix == '.odt':
-    infile = Path(sys.argv[1])
-else:
-    print("Error: Need to pass an ODT file as the first argument.")
-    exit(1)
+    # Ensure that file exists.
+    if infile.is_file() and infile.suffix == '.odt':
+        infile = infile.resolve()
+        # Load content.
+        doc = load(infile)
+    else:
+        print("Error: Input file does not exist.")
+        exit(1)
 
-# Ensure that file exists.
-if zipfile.is_zipfile(infile):
-    infile = infile.resolve()
-    # Get XML tree content.xml from ODT file.
-    with zipfile.ZipFile(infile, 'r') as odt:
-        with odt.open('content.xml', 'r') as content:
-            tree = ET.parse(content)
-else:
-    print("Error: Input file does not exist.")
-    exit(1)
+    # Copy infile to outfile, asking for confirmation if it exists.
+    langstr = f"__{'__'.join(languages)}"
+    outfile = infile.with_name(f"{infile.stem}{langstr}{infile.suffix}")
+    if outfile.is_file():
+        answer = input(f"\n{outfile} already exists. Replace it? [Y/n]: ")
+        try:
+            if answer.strip()[0].lower() == 'n':
+                exit(0)
+        except IndexError:
+            # User hit [Enter] with no text. Accept default overwrite.
+            pass
+    shutil.copyfile(infile, outfile)
 
-# Copy infile to outfile, asking for confirmation if it exists.
-langstr = f"__{'__'.join(languages)}"
-outfile = infile.with_name(f"{infile.stem}{langstr}{infile.suffix}")
-if outfile.is_file():
-    answer = input(f"\n{outfile} already exists. Replace it? [Y/n]: ")
-    try:
-        if answer.strip()[0].lower() == 'n':
-            exit(0)
-    except IndexError:
-        # User hit [Enter] with no text. Accept default overwrite.
-        pass
+    # Get hunspell dictionaries.
+    hs_dics = get_hs_dics(dict_dir, languages)
 
-shutil.copyfile(infile, outfile)
+    # Update XML tree.
+    print(f"\nDetermining the language of each paragraph...")
+    doc = update_autostyles(doc, hs_dics.keys())
+    doc, results = update_paragraphs_styles(doc, hs_dics)
 
-# # Build word lists.
-# print(f"\nBuilding word lists for {', '.join(languages)}...")
-# language_words_dict = {}
-# for lang_code in languages:
-#     # Build word lists.
-#     language_words_dict[lang_code] = hs.build_wordlist(dict_dir, lang_code)
-#     print(f"{lang_code}: {len(language_words_dict[lang_code])} words")
+    # Write out the updated file.
+    doc.save(outfile)
 
-# Get hunspell dictionaries.
-hs_dics = get_hs_dics(dict_dir, languages)
+    # Print summary data.
+    print_summary(results, hs_dics)
+    #print_results(results, hs_dics, start=10, end=30)
 
-# Update XML tree.
-print(f"\nDetermining the language of each paragraph...")
-tree, results = update_xml(tree, hs_dics)
 
-# Write out the updated file.
-update_zip(outfile, 'content.xml', tree)
-
-# Print summary data.
-print_summary(results)
-#print_results(results, start=10, end=30)
+if __name__ == '__main__':
+    main()
