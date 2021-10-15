c2dd96b3d511f81100d343fdecfa0b18829c7746
Nate Marti <nate_marti@sil.org>
2021-10-15 14:51:33 +0100

    remove nbsp & fix pg. numbers

--- clean-up-text.sh
+++ clean-up-text.sh
@@ -18,11 +18,15 @@ replace() {
 
 replace_nth_occurrance() {
     local n="$1"
-    local pat="$2"
+    local f="$2"
+    local r="$3"
 
-    echo "$contents" | sed -r ":a;N;\$!ba; ${pat}${n}" > "$outfile"
+    echo "$contents" | sed -r ":a;N;\$!ba; s@${f}@${r}@${n}" > "$outfile"
     contents=$(cat "$outfile")
 }
+
+# Remove non-breaking spaces that begin lines.
+replace '^\xC2\xA0'
 
 # Remove work records ##/##/##.
 replace '[0-9]{1,2}/[0-9]{1,2}/[0-9]{1,2}.*$'
@@ -37,13 +41,19 @@ replace '^[A-Z ]+$'
 replace '[A-Z0-9 ]{5,}'
 
 # Fix out-of-order chapter 254.
-replace_nth_occurrance 2 's/P254/P750/'
+replace_nth_occurrance 2 'P254' 'P750'
 
 # Fix poorly-formatted page numbers.
-replace '^[\s]*p([0-9]{2,3})' 'P\1'
+replace '^p([0-9]{2,3})' 'P\1'
 
-# Remove non-breaking spaces that begin lines.
-replace '^\xC2\xA0'
+# Fix mis-numbered page numbers.
+replace 'P318' 'P320'
+replace 'P317' 'P319'
+replace_nth_occurrance 2 'P316' 'P318'
+replace_nth_occurrance 2 'P315' 'P317'
+replace_nth_occurrance 2 'P563' 'P564'
+replace 'P748' 'P749'
+replace_nth_occurrance 2 'P747' 'P748'
 
 # Remove extra blank lines.
 perl -0777pi -e 's/\n{2,}/\n/g' "$outfile"
