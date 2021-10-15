a345531da1fef064aec85148777ced54f015b629
Nate Marti <nate_marti@sil.org>
2021-10-15 15:05:27 +0100

    ask if user wants to keep temp file

--- convert-txt-2-sfm.sh
+++ convert-txt-2-sfm.sh
@@ -6,6 +6,7 @@ if [[ "${infile: -16}" != '_en-US_clean.txt' ]] && [[ "${infile: -16}" != '_sg-C
     echo "Error: file name should end with \"_en-US_clean.txt\""
     exit 1
 fi
+temp="${infile}.tmp"
 stem="${infile%.txt}"
 part="${stem%_*}"
 lang="${part##*_}"
@@ -26,13 +27,11 @@ outfile="${HOME}/Paratext8Projects/${name}/${filename}"
 contents=$(cat "$infile")
 
 # Add ID.
-temp="${infile}.i"
 contents="\id XXA - Action Bible ($lang)
 $contents"
 echo "$contents" > "$temp"
 
 # Add chapter markers.
-temp="${temp}c"
 echo "$contents" | sed -r 's/P([0-9]+)/\\c \1/' > "$temp"
 contents=$(cat "$temp")
 # Fix out-of-order chapter 254.
@@ -40,12 +39,10 @@ echo "$contents" | sed -r ':a;N;$!ba; s/\\c 254/\\c 750/2' > "$temp"
 contents=$(cat "$temp")
 
 # Add verse markers.
-temp="${temp}v"
 echo "$contents" | sed -r 's/'"$panel"'\s*([0-9]+)/\\v \1/' > "$temp"
 contents=$(cat "$temp")
 
 # Add paragraph markers.
-temp="${temp}p"
 echo "$contents" | sed -r 's/^([^\\])/\\p \1/' > "$temp"
 contents=$(cat "$temp")
 
@@ -60,3 +57,10 @@ if [[ -e "$outfile" ]]; then
 else
     echo "$contents" > "$outfile"
 fi
+
+# Ask to keep temp file.
+read -p "Keep $temp? [y/N]: " ans
+if [[ "$ans" == 'y' ]] || [[ "$ans" == 'Y' ]]; then
+    exit 0
+fi
+rm -f "$temp"
