06481cfbc404f00d46f6280ff2c586572e2609a2
Nate Marti <nate_marti@sil.org>
2021-11-11 18:07:07 +0100

    clean out unused code

--- show-character-encoding.py
+++ show-character-encoding.py
@@ -3,19 +3,14 @@
 import sys
 import unicodedata
 
-# print(sys.stdin.read())
 charstr = sys.stdin.read().rstrip('\n')
 chars = [c for c in charstr]
-for c in chars:
+for i, c in enumerate(chars):
     cname = unicodedata.name(c, None)
-    if cname:
-        cchar = unicodedata.lookup(cname)
-    cunicode = c.encode('UTF-8').decode()
     cnfd = unicodedata.decomposition(c)
     is_nfc = True
     if not cnfd:
         cnfd = None
         is_nfc = False
 
-    # is_nfc = unicodedata.is_normalized('NFC', c)
-    print(f"{c}: NFC?: {is_nfc}, NFD: {cnfd}, {cname}")
+    print(f"{i}:  {c}\t{hex(ord(c))}\tNFC?: {is_nfc}, name: {cname}")
